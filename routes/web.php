e8944b6cf88998a26ddfb15696f92fa50ad5cebc
Ntim Yeboah <ntimyeboah@Ntims-MBP.mshome.net>
2017-05-20 10:46:24 +0000

    Get rid of cart route

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['as' => 'home'], function() {
+    Route::get('/', ['as' => 'index', 'uses' => 'HomeController@index']);
 });
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/cart', 'CartController@index')->name('cart');
