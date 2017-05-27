fac336cc0221cb90bd351185d14f63ae3ed5800b
Ntim Yeboah <ntimyeboah.obed@callenssolutions.com>
2017-05-27 18:28:41 +0000

    Compose auth user through all views

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        view()->composer(['*', 'layouts.dashboard'], AuthUserComposer::class);
+        view()->composer(['*'], AuthUserComposer::class);
     }
 
     /**
