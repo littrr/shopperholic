d21e4cee6eb7f4efbfc5017a11d9bbcd3046c13c
Ntim Yeboah <ntimyeboah.obed@callenssolutions.com>
2017-05-27 18:20:28 +0000

    Register and login users

--- resources/views/auth/includes/_login_and_register_modal.blade.php
+++ resources/views/auth/includes/_login_and_register_modal.blade.php
@@ -1,3 +1,4 @@
+{{--Sign in modal--}}
 <div class="modal signUpContent fade" id="ModalLogin" tabindex="-1" role="dialog">
     <div class="modal-dialog">
         <div class="modal-content">
@@ -6,40 +7,62 @@
                 <h3 class="modal-title-site text-center"> Sign in to {{ config('app.name') }} </h3>
             </div>
             <div class="modal-body">
-                <div class="form-group login-username">
-                    <div>
-                        <input name="log" id="login-user" class="form-control input" size="20" placeholder="Enter Username" type="text">
+                <form role="form" method="POST" action="{{ route('login') }}">
+                    {{ csrf_field() }}
+
+                    <div class="form-group login-username">
+                        <div>
+                            <input id="email" type="email" placeholder="Email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+                        </div>
+                        @if ($errors->has('email'))
+                            <span class="help-block">
+                                 <strong>{{ $errors->first('email') }}</strong>
+                            </span>
+                        @endif
                     </div>
-                </div>
-                <div class="form-group login-password">
-                    <div>
-                        <input name="Password" id="login-password" class="form-control input" size="20" placeholder="Password" type="password">
+
+                    <div class="form-group login-password">
+                        <div>
+                            <input id="password" type="password" placeholder="Password" class="form-control" name="password" required>
+                        </div>
+
+                        @if ($errors->has('password'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('password') }}</strong>
+                            </span>
+                        @endif
                     </div>
-                </div>
-                <div class="form-group">
-                    <div>
-                        <div class="checkbox login-remember">
-                            <label>
-                                <input name="rememberme" value="forever" checked="checked" type="checkbox">
-                                Remember Me
-                            </label>
+                    <div class="form-group">
+                        <div>
+                            <div class="checkbox login-remember">
+                                <label>
+                                    <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+                                </label>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div>
                     <div>
-                        <input name="submit" class="btn  btn-block btn-lg btn-primary" value="LOGIN" type="submit">
+                        <div>
+                            <button type="submit" class="btn btn-block btn-lg btn-primary">
+                                Login
+                            </button>
+                        </div>
                     </div>
-                </div>
+
+                </form>
             </div>
             <div class="modal-footer">
                 <p class="text-center"> Not here before? <a data-toggle="modal" data-dismiss="modal" href="master.blade.php#ModalSignup"> Sign Up. </a> <br>
-                    <a href="forgot-password.html"> Lost your password? </a>
+                    <a href="{{ route('password.request') }}">
+                        Lost your password?
+                    </a>
                 </p>
             </div>
         </div>
     </div>
 </div>
+
+{{--Sign up modal--}}
 <div class="modal signUpContent fade" id="ModalSignup" tabindex="-1" role="dialog">
     <div class="modal-dialog">
         <div class="modal-content">
@@ -48,41 +71,44 @@
                 <h3 class="modal-title-site text-center"> SIGNUP </h3>
             </div>
             <div class="modal-body">
-                <div class="control-group">
-                    <a class="fb_button btn  btn-block btn-lg " href="master.blade.php#">
-                        SIGNUP WITH FACEBOOK
-                    </a>
-                </div>
-                <h5 style="padding:10px 0 10px 0;" class="text-center"> OR </h5>
-                <div class="form-group reg-username">
-                    <div>
-                        <input name="login" class="form-control input" size="20" placeholder="Enter Username" type="text">
+                <form role="form" method="post" action="{{ route('register') }}">
+                    {{ csrf_field() }}
+                    <div class="form-group reg-username">
+                        <input id="name" type="text" placeholder="Full Name" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+
+                        @if ($errors->has('name'))
+                            <span class="help-block">
+                                 <strong>{{ $errors->first('name') }}</strong>
+                            </span>
+                        @endif
                     </div>
-                </div>
-                <div class="form-group reg-email">
-                    <div>
-                        <input name="reg" class="form-control input" size="20" placeholder="Enter Email" type="text">
+                    <div class="form-group reg-email">
+                        <input id="email" type="email" placeholder="Email" class="form-control" name="email" value="{{ old('email') }}" required>
+
+                        @if ($errors->has('email'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('email') }}</strong>
+                            </span>
+                        @endif
                     </div>
-                </div>
-                <div class="form-group reg-password">
-                    <div>
-                        <input name="password" class="form-control input" size="20" placeholder="Password" type="password">
+                    <div class="form-group reg-password">
+                        <input id="password" type="password" placeholder="Password" class="form-control" name="password" required>
+
+                        @if ($errors->has('password'))
+                            <span class="help-block">
+                                 <strong>{{ $errors->first('password') }}</strong>
+                            </span>
+                        @endif
                     </div>
-                </div>
-                <div class="form-group">
-                    <div>
-                        <div class="checkbox login-remember">
-                            <label>
-                                <input name="rememberme" id="rememberme" value="forever" checked="checked" type="checkbox">
-                                Remember Me </label>
+                    <div class="form-group">
+                        <div>
+                            <input id="password-confirm" type="password" placeholder="Retype Password" class="form-control" name="password_confirmation" required>
                         </div>
                     </div>
-                </div>
-                <div>
                     <div>
-                        <input name="submit" class="btn  btn-block btn-lg btn-primary" value="REGISTER" type="submit">
+                        <button type="submit" class="btn btn-block btn-lg btn-primary">Register</button>
                     </div>
-                </div>
+                </form>
             </div>
             <div class="modal-footer">
                 <p class="text-center">
